c7e2fe4a981579db65901432598c78feafd67abb
marcelopilone <mdpilone@gmail.com>
2019-10-23 16:46:55 -0300

    commit en casa

--- main.py
+++ main.py
@@ -5,17 +5,44 @@ from kivymd.uix.navigationdrawer import NavigationDrawerIconButton
 from kivymd.theming import ThemeManager
 from kivymd.toast import toast
 
+main_kv = """
+<ContentNavigationDrawer@MDNavigationDrawer>:
+    drawer_logo: 'demos/kitchen_sink/assets/drawer_logo.png'
+
+    NavigationDrawerSubheader:
+        text: "Menu:"
+
+
+NavigationLayout:
+    id: nav_layout
+
+    ContentNavigationDrawer:
+        id: nav_drawer
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        MDToolbar:
+            id: toolbar
+            title: 'KivyMD Kitchen Sink'
+            md_bg_color: app.theme_cls.primary_color
+            background_palette: 'Primary'
+            background_hue: '500'
+            elevation: 10
+            left_action_items:
+                [['dots-vertical', lambda x: app.root.toggle_nav_drawer()]]
+
+        Widget:
+"""
+
 
 class Example(App):
     theme_cls = ThemeManager()
     theme_cls.primary_palette = 'Teal'
-    title = "Menu"
-
-    
 
     def build(self):
-        return ContentNavigationDrawerMDNavigationDrawer()
-
+        self.main_widget = Builder.load_string(main_kv)
+        return self.main_widget
 
     def callback(self, instance, value):
         toast("Pressed item menu %d" % value)
@@ -24,11 +51,8 @@ class Example(App):
         for i in range(15):
             self.main_widget.ids.nav_drawer.add_widget(
                 NavigationDrawerIconButton(
-                    icon='checkbox-blank-circle', text="El link %d" % i,
+                    icon='checkbox-blank-circle', text="Item menu %d" % i,
                     on_release=lambda x, y=i: self.callback(x, y)))
-
-class ContentNavigationDrawerMDNavigationDrawer():
-    	pass
 
 
 Example().run()
